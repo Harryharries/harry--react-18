90ec04d12d921f83af03c58a737692c78e914fbd
HarryharrGu <harry.guapi@gmail.com>
2024-03-04 13:23:33 -0700

    UI complete the restaurant from

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -8,7 +8,7 @@ import ProtectedRoute from "./auth/ProtectedRoute";
 // import HomePage from "./pages/HomePage";
 // import AuthCallbackPage from "./pages/AuthCallbackPage";
 // import ProtectedRoute from "./auth/ProtectedRoute";
-// import ManageRestaurantPage from "./pages/ManageRestaurantPage";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 // import SearchPage from "./pages/SearchPage";
 // import DetailPage from "./pages/DetailPage";
 // import OrderStatusPage from "./pages/OrderStatusPage";
@@ -20,7 +20,14 @@ const AppRoutes = () => {
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
       <Route element={<ProtectedRoute />}>
         <Route path="/user-profile" element={<Layout><UserProfilePage/></Layout>} />
-
+        <Route
+          path="/manage-restaurant"
+          element={
+            <Layout>
+              <ManageRestaurantPage />
+            </Layout>
+          }
+        />
       </Route>
       <Route path="*" element={<Navigate to="/" />} />
       
