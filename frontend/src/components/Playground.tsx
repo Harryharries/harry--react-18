6781c357d7d57c8d710abc81906a0c45f274acdd
HarryharrGu <harry.guapi@gmail.com>
2024-03-05 14:18:23 -0700

    add more case

--- frontend/src/components/Playground.tsx
+++ frontend/src/components/Playground.tsx
@@ -5,8 +5,9 @@
 // import { User, fetchUsers } from "@/utils/utils";
 // import Search from "./ui/test/Search";
 // import { useDebounce } from "@/hooks/useDebounce";
-import { Provider } from "@/services/providers/test/MyContext";
-import Parent from "./ui/test/context/Parent";
+// import { Provider } from "@/services/providers/test/MyContext";
+// import Parent from "./ui/test/context/Parent";
+import { useEffect, useState } from "react";
 
 const Playground = () => {
 
@@ -83,13 +84,76 @@ const Playground = () => {
 
 // context
 
+// return (
+//     <Provider>
+//         <div className="container mx-auto">
+//             <Parent></Parent>
+//         </div>
+//     </Provider>
+//     );
+
+//effect and render and state
+
+// const [count, setCount] = useState(0);
+// const countr = useRef(0);
+
+// useEffect(() => {
+//   // The code that we want to run
+//   console.log('The count is:', count);
+
+//   // Optional return function
+//   return () => {
+//     console.log('I am being cleaned up!');
+//   };
+// }, [count]); // The dependency array
+
+// return (
+//   <div className='tutorial'>
+//     <h1>Count: {count}</h1>
+//     <button onClick={() => setCount(count - 1)}>
+//       Decrement
+//     </button>
+//     <button onClick={() => setCount(count + 1)}>
+//       Increment
+//     </button>
+//     <button onClick={() => countr.current++}>
+//       IncrementRef
+//     </button>
+//   </div>
+// );
+
+const [count, setCount] = useState(0);
+const bad = {userId:count}
+
+useEffect(() => {
+  // The code that we want to run
+  console.log('The count is:', count);
+
+  // Optional return function
+  return () => {
+    console.log('I am being cleaned up!');
+  };
+}, [count]); // The dependency array
+
+useEffect(() => {
+    console.log('bad:', bad.userId);
+    bad.userId = 2
+  
+  }, [bad]); 
+
 return (
-    <Provider>
-        <div className="container mx-auto">
-            <Parent></Parent>
-        </div>
-    </Provider>
-    );
+  <div className='tutorial'>
+    <h1>Count: {count}</h1>
+    <button onClick={() => setCount(count - 1)}>
+      Decrement
+    </button>
+    <button onClick={() => setCount(count + 1)}>
+      Increment
+    </button>
+  </div>
+);
+
+
 };
 
 export default Playground;
